9bc4b7400ee05b1e840f8b7b736780dc97781e03
Pauld <polichinel8@gmail.com>
2017-11-14 23:27:09 +0100

    Ajoutez des fichiers projet.

---
+++ Todo.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Content.PM;
+using Android.Views;
+
+namespace Todo
+{
+	[Activity(Label = "Todo", Icon = "@drawable/icon", MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity :
+	global::Xamarin.Forms.Platform.Android.FormsApplicationActivity // superclass new in 1.3
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App());
+
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop) {
+                Window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+                Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+                Window.SetStatusBarColor(Android.Graphics.Color.Rgb(194, 24, 91)); }
+        }
+	}
+}
